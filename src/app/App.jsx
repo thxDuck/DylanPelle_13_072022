679c0dc2c5f2e624f5825f17e9c3634ea8bc2389
Dylan Pellé <dylan.pelle.dev@gmail.com>
2022-07-21 11:27:21 +0200

    Initialize react-redux, successfull add user state

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import Header from "../components/partials/Header";
 import Footer from "../components/partials/Footer";
 import RoutesApp from "./Routes.jsx";
 import "../styles/style.css";
+import store from "../utils/store";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <RoutesApp />
-            <Footer />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Header />
+                <RoutesApp />
+                <Footer />
+            </BrowserRouter>
+        </Provider>
     );
 };
 export default App;
