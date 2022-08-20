b53b01d1ce7156a5b2b2444cb8fbcbb5629b8471
Dylan Pellé <dylan.pelle.dev@gmail.com>
2022-08-20 17:40:39 +0200

    Add swagger-ui-react(errors with this libs), Adding DELETE requests

--- src/app/Routes.jsx
+++ src/app/Routes.jsx
@@ -1,4 +1,8 @@
 import { Routes, Route } from "react-router-dom";
+
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
+
 import Home from "../pages/Home";
 import Login from "../pages/Login.jsx";
 import Profile from "../pages/Profile.jsx";
@@ -8,6 +12,7 @@ const RoutesApp = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/api-doc" element={<SwaggerUI url="./swagger.yaml" />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<Error />} />
